df2bba9ee094c79a21182d282afb01a19fceeeae
hello <3034341736@qq.com>
2015-01-23 00:18:12 +0800

    add Emp.java

--- Emp.java
+++ Emp.java
@@ -1,6 +1,12 @@
+import java.uitl.*;
 public class Emp
 {
 	private Integer empNo;
 	private String empName;
 	private Integer depId;
+	private Double sal;
+	private Date birth;
+	public void toString(){
+		System.out.println("一位雇员");
+	}
 }
